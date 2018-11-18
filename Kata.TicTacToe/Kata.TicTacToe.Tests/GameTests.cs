b287b3c214221b56fc024e1d5d476c1aeace4430
deimors <deimors@gmail.com>
2018-11-18 10:12:54 -0600

    Act first; Red, Not Implemented
    . Create the empty Tests
    
    . Begin the first Fact
    
    . Describe the first Act in the first Fact
    
    . Abstract the Test Fixture
    
    . Declare the private Implementation
    
    . Delegate the first Act to the Implementation
    
    . Declare the first Act as Not Implemented

---
+++ Kata.TicTacToe/Kata.TicTacToe.Tests/GameTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kata.TicTacToe.Tests
+{
+	public class GameTests : GameTestFixture
+	{
+		[Fact]
+		public void MarkFirstXInTopCorner()
+		{
+			Act_MarkX(0, 0);
+		}
+	}
+
+	public abstract class GameTestFixture
+	{
+		private readonly Game _game = new Game();
+
+		protected void Act_MarkX(int x, int y)
+			=> _game.MarkX(x, y);
+	}
+	
+	public class Game
+	{
+		public void MarkX(int x, int y)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
