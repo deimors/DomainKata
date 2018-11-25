2131c7c0e20d36d2e76cda765ae36c8555336429
deimors <deimors@gmail.com>
2018-11-25 13:22:40 -0600

    Introduce "whose turn is it?" state; Green

--- Kata.TicTacToe/Kata.TicTacToe/Game.cs
+++ Kata.TicTacToe/Kata.TicTacToe/Game.cs
@@ -9,19 +9,20 @@ namespace Kata.TicTacToe
 	{
 		private readonly ISubject<GameEvent> _events = new Subject<GameEvent>();
 
+		private bool _isXTurn = true;
+
 		public IObservable<GameEvent> Events => _events;
 
 		public Result<Unit, GameError> MarkX(int x, int y) 
 			=> Result
 				.Create(IsMarkInsideBoard(x, y), () => Unit.Value, () => GameError.MarkOutsideBoard)
+				.Do(_ => _isXTurn = false)
 				.Do(_ => _events.OnNext(new XMarkedEvent(x, y)));
+
+		public Result<Unit, GameError> MarkO(int x, int y) 
+			=> Result.Create(!_isXTurn, () => Unit.Value, () => GameError.OutOfOrderMark);
 
 		private static bool IsMarkInsideBoard(int x, int y)
 			=> x >= 0 && x <= 2 && y >= 0 && y <= 2;
-
-		public Result<Unit, GameError> MarkO(int x, int y)
-		{
-			return Result.Failure<Unit, GameError>(GameError.OutOfOrderMark);
-		}
 	}
 }
