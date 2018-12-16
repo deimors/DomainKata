0e1a1986a7bcff81ed59000c63fae91b296dbfdf
deimors <deimors@gmail.com>
2018-12-16 11:45:43 -0600

    Split DoorTests into LockedDoorTests, UnlockedDoorTests and DoorTestFixture [Refactor]

--- Kata.LockedDoor/Kata.LockedDoor/Kata.LockedDoor.Tests/DoorTests.cs
+++ Kata.LockedDoor/Kata.LockedDoor/Kata.LockedDoor.Tests/DoorTests.cs
@@ -6,47 +6,55 @@ using Xunit;
 
 namespace Kata.LockedDoor.Tests
 {
-	public class DoorTests
+	public class DoorTestFixture
 	{
-		private Door _door;
-		private readonly IObserver<DoorOpenedEvent> _observer = A.Fake<IObserver<DoorOpenedEvent>>();
+		protected readonly Door _door;
+		protected readonly IObserver<DoorOpenedEvent> _observer = A.Fake<IObserver<DoorOpenedEvent>>();
+
+		protected DoorTestFixture(Door door)
+		{
+			_door = door;
+			_door.Subscribe(_observer);
+		}
+
+		protected void Assert_EventNotObserved<T>() where T : DoorOpenedEvent
+		{
+			A.CallTo(() => _observer.OnNext(A<T>._)).MustNotHaveHappened();
+		}
+
+		protected void Assert_EventObserved(DoorOpenedEvent expectedEvent)
+		{
+			A.CallTo(() => _observer.OnNext(expectedEvent)).MustHaveHappened();
+		}
+	}
+
+	public class LockedDoorTests : DoorTestFixture
+	{
+		public LockedDoorTests() : base(new Door(locked: true)) {}
 
 		[Fact]
 		public void TryToOpenLockedDoor()
 		{
-			_door = new Door(locked: true);
-
-			_door.Subscribe(_observer);
-
 			var result = _door.Open();
 
 			result.Should().Be(Result.Failure<Unit, DoorError>(DoorError.CantOpenLockedDoor));
 
 			Assert_EventNotObserved<DoorOpenedEvent>();
 		}
+	}
 
-		private void Assert_EventNotObserved<T>() where T : DoorOpenedEvent
-		{
-			A.CallTo(() => _observer.OnNext(A<T>._)).MustNotHaveHappened();
-		}
+	public class UnlockedDoorTests : DoorTestFixture
+	{
+		public UnlockedDoorTests() : base(new Door(locked: false)) {}
 
 		[Fact]
 		public void TryToOpenUnlockedDoor()
 		{
-			_door = new Door(locked: false);
-
-			_door.Subscribe(_observer);
-
 			var result = _door.Open();
 
 			result.Should().Be(Result.Success<Unit, DoorError>(Unit.Value));
 
 			Assert_EventObserved(new DoorOpenedEvent());
-		}
-
-		private void Assert_EventObserved(DoorOpenedEvent expectedEvent)
-		{
-			A.CallTo(() => _observer.OnNext(expectedEvent)).MustHaveHappened();
 		}
 	}
 }
